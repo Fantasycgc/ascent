7621413c22fec5236747f3a5ad97561b1bf5a17f
hn.tien <hntien248@yahoo.com>
2024-09-30 06:04:19 +0700

    Add section service

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,38 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    container:{
-      center:true
+    container: {
+      center: true
     },
     extend: {
-      fontFamily:{
-        "nunito":['Nunito', 'system-ui'],
-        "Bubblegum-Sans":["Bubblegum Sans", 'sans-serif']
+      fontFamily: {
+        "nunito": ['Nunito', 'system-ui'],
+        "Bubblegum-Sans": ["Bubblegum Sans", 'sans-serif'],
+        "jost": ["Jost", 'sans-serif']
+      },
+      animation: {
+        "anime-img": "anime-img 20s linear 0s 9999999999 normal both",
+      },
+      keyframes: {
+        "anime-img": {
+          "0%,100%": {
+            transform: "translateX(0)",
+          },
+
+          "10%,30%,50%,70%": {
+            transform: "translateX(-10px)",
+          },
+
+          "20%,40%,60%": {
+            transform: "translateX(10px)",
+          },
+          "80%": {
+            transform: "translateX(8px)",
+          },
+          "90%": {
+            transform: "translateX(-8px)",
+          }
+        }
       }
     },
   },
